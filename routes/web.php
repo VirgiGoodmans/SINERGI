3241b5cac2b33eeb22aa916cb60c7e8849a7eade
Virgi Shaw <virgishaw@gmail.com>
2024-11-16 18:36:51 +0700

    scnd

--- routes/web.php
+++ routes/web.php
@@ -49,8 +49,19 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
 // Rute untuk halaman wisata
 Route::prefix('wisata')->group(function () {
+    // Wisata Siblarak
     Route::get('/siblarak', [WisataController::class, 'showSiblarak'])->name('wisata.siblarak');
+    Route::post('/siblarak/pemesanan', [WisataController::class, 'storeSiblarak'])->name('wisata.siblarak.pemesanan');
+
+    // Wisata Kemanten
     Route::get('/kemanten', [WisataController::class, 'showKemanten'])->name('wisata.kemanten');
+    Route::post('/kemanten/pemesanan', [WisataController::class, 'storeKemanten'])->name('wisata.kemanten.pemesanan');
+
+    // Wisata Kampung Dolanan
     Route::get('/kampung-dolanan', [WisataController::class, 'showKampungDolanan'])->name('wisata.kampung-dolanan');
+    Route::post('/kampung-dolanan/pemesanan', [WisataController::class, 'storeKampungDolanan'])->name('wisata.kampung-dolanan.pemesanan');
+
+    // UMKM Lokal
     Route::get('/umkm-lokal', [WisataController::class, 'showUmkmLokal'])->name('wisata.umkm-lokal');
+    Route::post('/umkm-lokal/pemesanan', [WisataController::class, 'storeUmkmLokal'])->name('wisata.umkm-lokal.pemesanan');
 });
